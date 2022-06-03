1f9149349bbff1cc5401647310ccddeeadc5e65e
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-06-03 09:13:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,8 +8,8 @@
 std::vector<char> getPerm(const Tree& tree, int n) {
  std::string origin = tree[n - 1];
  std::vector<char> prm;
-  for (int l = 0; l < origin.length(); l++) {
-    prm.push_back(origin[l]);
-  }
-  return prm;
+ for (int l = 0; l < origin.length(); l++) {
+  prm.push_back(origin[l]);
+ }
+ return prm;
 }
